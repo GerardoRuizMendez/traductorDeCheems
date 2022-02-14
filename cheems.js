5e01517128ea2704737233748328b4d932af092e
Ruiz Mendez Gerardo <gerardoruizmendez012@gmail.com>
2022-02-14 12:49:28 -0600

    Corrigiendo malas practicas

--- cheems.js
+++ cheems.js
@@ -1,31 +1,20 @@
-var FPS = 50;
-var area;
-var palabras=[];
-var boton;
-var texto;
-var mitad;
-var etiqueta;
-function inicio(){
-    area=document.getElementById('area2').style.display="block";
-}
-function accion(){
-    texto=document.getElementById('area').value;
-    palabras=texto.split(" ");
+function accion() {
+    var mitad;
 
-    texto="";
-    for(var i=0;i<palabras.length; i++){
-        mitad=palabras[i].length/2;
-        mitad=Math.trunc(mitad);
-        for(var j=0; j<palabras[i].length; j++){
-           texto=texto+palabras[i].charAt(j);
-           if(j==mitad){
-            texto=texto+'m';
-           }
+    var palabras = document.getElementById('area').value;
+    palabras = palabras.split(" ");
+
+    var texto = "";
+    for (var i = 0; i < palabras.length; i++) {
+        mitad = palabras[i].length / 2;
+        mitad = Math.trunc(mitad);
+        for (var j = 0; j < palabras[i].length; j++) {
+            texto = texto + palabras[i].charAt(j);
+            if (j == mitad) {
+                texto = texto + 'm';
+            }
         }
-        texto+=' ';
+        texto += ' ';
     }
-    etiqueta=document.getElementById('etiqueta');
-    etiqueta.innerHTML=texto;
-    area.style.display="none";
-    
+    document.getElementById('etiqueta').innerHTML = texto;
 }
